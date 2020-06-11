44e063c79d5b42cae945c98d2e76e16b5e9d1a53
Piyush Gupta <iopiyushg@gmail.com>
2020-06-11 19:52:29 +0530

    Minor Updates, No Design Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hidato/screens/loading_screen.dart';
+import 'package:hidato/screens/home_screen.dart';
+import 'package:hidato/screens/puzzle_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,18 +10,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: MaterialApp(
-        themeMode: ThemeMode.system,
-        theme: ThemeData.light(),
-        darkTheme: ThemeData.dark(),
-        home: '/',
-        routes: {
-          '/': (context) => LoadingScreen(),
-          '/home': (context) => HomeScreen(),
-          '/puzzle': (context) => PuzzleScreen(),
-        },
-      ),
+    return MaterialApp(
+      themeMode: ThemeMode.system,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => LoadingScreen(),
+        '/home': (context) => HomeScreen(),
+        '/puzzle': (context) => PuzzleScreen(),
+      },
     );
   }
 }
